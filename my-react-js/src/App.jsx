deb2ae93c023ca4c20df1bb971fe9a1f0d91a7ec
JOHN PATRICK S. AWIT <awitjohnpatrick@gmail.com>
2024-05-01 20:22:55 +0800

    reactjs

---
+++ my-react-js/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return <div className='w-full h-screen text-4xl text-blue-700 flex items-center justify-center'>
+    HELLO, I'M JOHN PATRICK AWIT
+  </div>
+}
+
+export default App
